8d7cb8d8d8f83b4f69b91bc80b1ecdc02e65502b
Zach <zrfox97@gmail.com>
2025-06-18 18:11:59 -0700

    Add pressable to GoalItem and delete onPress functionality.

--- App.js
+++ App.js
@@ -11,21 +11,39 @@ export default function App() {
   function addGoalHandler(enteredGoalText) {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals, 
-      {text: enteredGoalText, key: Math.random().toString()}, // Object is used to include key for each goal while using FlatList for lazy loading. 
+      {text: enteredGoalText, id: Math.random().toString()}, // Object is used to include key for each goal while using FlatList for lazy loading. 
     ]);
   }
+
+function deleteGoalHandler(id) {
+  setCourseGoals(currentCourseGoals => {
+    return (currentCourseGoals.filter((goal) => goal.id !== id ))
+  } )
+}
+
   return (
     <View style={styles.appContainer}>
       <GoalInput onAddGoal={addGoalHandler} />
       <View  style={styles.goalsContainer}> 
       <FlatList data={courseGoals} renderItem={(itemData) =>{ 
-        return <GoalItem text={itemData.item.text} />
+        return (
+          <GoalItem 
+            text={itemData.item.text}
+            id={itemData.item.id} 
+            onDeleteItem={deleteGoalHandler} 
+            />
+      );
       }}
+      keyExtractor={(item, index) => {
+        return item.id;
+        
+      }}
+      
        />
       </View>
     </View>
 
-  );
+    )
 }
 
 const styles = StyleSheet.create({
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,10 +1,16 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
 
 function GoalItem (props) {
     return (
+      <Pressable 
+        android_ripple={{ color: '#dddddd'}}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({pressed}) => pressed && styles.pressedItem}  
+      >
         <View style={styles.goalItem}> 
-            <Text>{props.text}</Text>  
+            <Text style={styles.goalText}>{props.text}</Text>  
           </View>
+      </Pressable>
     );
 }
 
@@ -17,5 +23,12 @@ const styles = StyleSheet.create({
     borderRadius: 6,
     backgroundColor: '#5e0acc',
     color: 'white',
+  },
+  pressedItem: {
+    opacity: 0.5
+  },
+  goalText: {
+    color: 'white',
+    padding: 8,
   }
 });
